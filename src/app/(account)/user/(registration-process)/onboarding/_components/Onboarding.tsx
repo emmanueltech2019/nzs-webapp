4226025926bf43df4cf4176669c4944bd54b2ab0
Itz Miracle <113153105+otobong23@users.noreply.github.com>
2024-09-18 10:15:17 +0000

    repository updated from codesandbox

---
+++ src/app/(account)/user/(registration-process)/onboarding/_components/Onboarding.tsx
@@ -0,0 +1,16 @@
+import { Icon } from "@iconify/react/dist/iconify.js"
+
+const chart = 'mage:chart-fill'
+const delivery = 'solar:delivery-bold'
+const verified_user = 'material-symbols:verified-user'
+
+const Onboading = () => {
+  return (
+    <section className="flex flex-col-reverse lg:flex-row">
+        <div className="col flex-1">col-1 text</div>
+        <div className="col flex-1">col-2 image</div>
+    </section>
+  )
+}
+
+export default Onboading
---
+++ src/app/(account)/user/(registration-process)/onboarding/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RouterBtn from "@/components/buttons/RouteBtn";
+
+function page() {
+  return (
+    <div>
+      <div className="m-[20px]">
+        <RouterBtn url="./" text="Next"/>
+      </div>
+    </div>
+  );
+}
+
+export default page;
