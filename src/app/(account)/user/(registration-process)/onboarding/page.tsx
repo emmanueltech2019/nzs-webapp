4cb56e37ed28166f67c80863d338d7c16dc2402f
Princewill Jeremiah <princewilljeremiah83@gmail.com>
2024-09-19 22:14:25 +0100

    verification

--- src/app/(account)/user/(registration-process)/onboarding/page.tsx
+++ src/app/(account)/user/(registration-process)/onboarding/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import RouterBtn from "@/components/buttons/RouteBtn";
 import Onboarding from "./_components/Onboarding";
 import FuncRouteBtn from "@/components/buttons/FuncRouteBtn";
 import Box from "@/components/Box";
