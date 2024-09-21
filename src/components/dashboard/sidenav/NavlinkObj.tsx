04fc02554006717c97eafe3bd7471c97c42a2b6f
Princewill Jeremiah <princewilljeremiah83@gmail.com>
2024-09-21 15:21:20 +0100

    right sidebar

--- src/components/dashboard/sidenav/NavlinkObj.tsx
+++ src/components/dashboard/sidenav/NavlinkObj.tsx
@@ -1,4 +1,5 @@
 import { Icon } from '@iconify/react'
+import { div } from 'framer-motion/client'
 
 export const links = [
   {
@@ -31,6 +32,57 @@ export const links = [
         <Icon icon="ri:settings-5-fill" className='text-[34px] text-[#D4D6DD]'></Icon>
         <p className='text-[19px] text-[#71727A] leading-[22.99px] font-[400]'>Settings</p>
       </div>
+    )
+  },
+]
+
+export const profileOptions = [
+  {
+    optionItem: (
+      <span className='p-[26px] flex items-center justify-between'>
+        <p className='text-[#1F2024] text-sm'>My Wallet</p>
+        <Icon icon="formkit:right" className='text-[20px] text-[#8F9098]'></Icon>
+      </span>
+    )
+  },
+  {
+    optionItem: (
+      <span className='p-[26px] flex items-center justify-between'>
+        <p className='text-[#1F2024] text-sm'>Inventory</p>
+        <Icon icon="formkit:right" className='text-[20px] text-[#8F9098]'></Icon>
+      </span>
+    )
+  },
+  {
+    optionItem: (
+      <span className='p-[26px] flex items-center justify-between'>
+        <p className='text-[#1F2024] text-sm'>Notifications</p>
+        <Icon icon="formkit:right" className='text-[20px] text-[#8F9098]'></Icon>
+      </span>
+    )
+  },
+  {
+    optionItem: (
+      <span className='p-[26px] flex items-center justify-between'>
+        <p className='text-[#1F2024] text-sm'>Language</p>
+        <Icon icon="formkit:right" className='text-[20px] text-[#8F9098]'></Icon>
+      </span>
+    )
+  },
+  {
+    optionItem: (
+      <span className='p-[26px] flex items-center justify-between'>
+        <p className='text-[#1F2024] text-sm'>Privacy &#38; Security</p>
+        <Icon icon="formkit:right" className='text-[20px] text-[#8F9098]'></Icon>
+      </span>
+    )
+  },
+  {
+    optionItem: (
+      <span className='p-[26px] flex items-center justify-between'>
+        <p className='text-[#1F2024] text-sm'>Log Out</p>
+        <Icon icon="formkit:right" className='text-[20px] text-[#8F9098]'></Icon>
+      </span>
     )
   },
 ]
--- src/components/dashboard/sidenav/RightSidenav.tsx
+++ src/components/dashboard/sidenav/RightSidenav.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Icon } from '@iconify/react'
+import { profileOptions } from './NavlinkObj'
+import { div } from 'framer-motion/client'
 
 const RightSidenav = () => {
   return (
-    <div className='hidden md:flex justify-between items-center flex-col sticky top-0 h-screen max-h-[1200px] w-3/12 max-w-[344px] py-[70px] ps-[35px]'>RightSidenav</div>
+    <div className='hidden bg-[#EAF2FF] md:flex justify-between items-center flex-col sticky top-0 h-screen max-h-[1200px] w-3/6 max-w-[344px] m-auto py-[60px] px-[32px]'>
+
+      {/* Profile */}
+      <div className='py-2 block justify-center items-center w-full'>
+        <div className='bg-[#E09427] w-[80px] h-[80px] m-auto rounded-[32px] overflow-hidden'>
+          <Icon icon="mingcute:user-2-fill" className='relative top-2 left-[1px] text-white text-[80px]'></Icon>
+        </div>
+
+        {/* brief info */}
+        <div className='text-center mt-4'>
+          <h3 className='font-black text-[#1F2024] text-base'>Lucas Scott</h3>
+          <p className='text-xs font-normal text-[#71727A]'>@lucasscott3</p>
+        </div>
+
+        {/* profile options */}
+        <div className='py-6 grid gap-[10px]'>
+          {
+            profileOptions.map((item, index) => (
+              <div key={index} className='w-full block border-t-[0.5px] border-[#D4D6DD]'>
+                {item.optionItem}
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
