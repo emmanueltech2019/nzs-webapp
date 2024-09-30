74a89433d4889ae64162321848627a561a7072d8
Emmanuel Lucky <emmanueltech2019@gmail.com>
2024-09-30 16:52:45 +0100

    updates

--- src/app/(account)/user/dashboard/transaction/page.tsx
+++ src/app/(account)/user/dashboard/transaction/page.tsx
@@ -18,17 +18,6 @@ const Transactions = () => {
   return (
     <div className="max-w-2xl mx-auto px-4 py-6">
       <TagHeader title='My Transactions'/>
-<<<<<<< HEAD
-     
-      {/* <div className="flex space-x-2 mb-4">
-        <button className="bg-green-600 text-white px-4 py-2 rounded-full">Popular</button>
-        <button className="bg-gray-100 px-4 py-2 rounded-full">Cost-efficient</button>
-        <button className="bg-gray-100 px-4 py-2 rounded-full">Location</button>
-        <button className="bg-gray-100 px-4 py-2 rounded-full">Anambra</button>
-      </div> */}
-      
-      <div>
-=======
 
       <TransactionTab activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab == 'incoming' ? 
@@ -41,7 +30,6 @@ const Transactions = () => {
       </div>) 
       : (
         <div>
->>>>>>> 514870276bc79d86360027c985c2f1996f737790
         {transactions.map((transaction, index) => (
           <TransactionCard
             key={index}
@@ -53,13 +41,7 @@ const Transactions = () => {
         ))}
       </div>
       )}
-     
-      {/* <div className="flex space-x-2 mb-4">
-        <button className="bg-green-600 text-white px-4 py-2 rounded-full">Popular</button>
-        <button className="bg-gray-100 px-4 py-2 rounded-full">Cost-efficient</button>
-        <button className="bg-gray-100 px-4 py-2 rounded-full">Location</button>
-        <button className="bg-gray-100 px-4 py-2 rounded-full">Anambra</button>
-      </div> */}
+
       
     </div>
   );
