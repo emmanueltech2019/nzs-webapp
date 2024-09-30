f37f9fc6301a0579baf0c0e722564fc9dc695729
Princewill Jeremiah <princewilljeremiah83@gmail.com>
2024-09-30 21:08:37 +0100

    update

--- src/app/(account)/user/dashboard/(services)/health/page.tsx
+++ src/app/(account)/user/dashboard/(services)/health/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import React, { useState } from 'react'
-
+import Health from './components/Health'
 
 const page = () => {  
 
 
   return (
     <div className="min-h-screen md:w-[61vw] ">
+      <Health />
     </div>
  )
 }
