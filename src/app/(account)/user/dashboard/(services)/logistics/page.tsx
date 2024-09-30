82c2ef9427546c98b20c6393de48515a38225e0b
Emmanuel Lucky <emmanueltech2019@gmail.com>
2024-09-30 17:15:07 +0100

    updated

---
+++ src/app/(account)/user/dashboard/(services)/logistics/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState } from 'react'
+
+
+const page = () => {  
+
+
+  return (
+    <div className="min-h-screen md:w-[61vw] ">
+    </div>
+ )
+}
+
+export default page
