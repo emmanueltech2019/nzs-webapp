9f40f9c27f17f661940b862491bf8cde988f17f2
Emmanuel Lucky <emmanueltech2019@gmail.com>
2024-10-01 01:37:58 +0100

    updated

--- src/app/(account)/user/dashboard/(services)/layout.tsx
+++ src/app/(account)/user/dashboard/(services)/layout.tsx
@@ -8,7 +8,6 @@ export default function layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className=" block">
-        
       <Header />
 
       <ProductAndServiceTab activeTab={activeTab} setActiveTab={setActiveTab} />
