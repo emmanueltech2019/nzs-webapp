f5b55a343f839d5731ec41d52b8f5410446fda5c
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-10-04 16:03:20 +0100

    repo updated

--- src/app/(account)/user/dashboard/add-item/components/Accordion.tsx
+++ src/app/(account)/user/dashboard/add-item/components/Accordion.tsx
@@ -16,16 +16,14 @@ const Accordion: React.FC<accordionPropsType> = ({ title, children, onClick, sta
   return (
     <div className='flex justify-between items-start py-4 border-b-[0.5px] border-[#D4D6DD]'>
       <div className="col">
-        <h2 className={`card-title`}>{title}</h2>
+        <div onClick={() => { setBatch(0); onClick() }} className='flex justify-between cursor-pointer'>
+          <h2 className={`card-title`}>{title}</h2>
+          {batchNum ? <div className="w-6 h-6 rounded-full flex justify-center items-center text-white text-[10px] bg-[--foreground-green]">{batchNum}</div>
+            : <Icon icon={`bi:chevron-down`} className={`font-bold transition-all duration-200 ${state ? 'rotate-180' : 'rotate-0'}`} />}
+        </div>
         <div className={`overflow-hidden transition-all duration-200 ${state ? 'mt-3 max-h-[1000px]' : 'mt-0 max-h-0'}`}>
           {children}
         </div>
-      </div>
-      <div className="col">
-        <button onClick={() => {setBatch(0); onClick()}}>
-          {batchNum ? <div className="w-6 h-6 rounded-full flex justify-center items-center text-white text-[10px] bg-[--foreground-green]">{batchNum}</div>
-          : <Icon icon={`bi:chevron-down`} className={`font-bold transition-all duration-200 ${state ? 'rotate-180' : 'rotate-0'}`} />}
-        </button>
       </div>
     </div >
   )
