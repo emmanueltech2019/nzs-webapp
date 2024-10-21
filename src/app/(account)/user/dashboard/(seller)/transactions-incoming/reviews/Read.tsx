6cd47067e26982c133baceddea34ccb1d72a3d8e
Princewill Jeremiah <princewilljeremiah83@gmail.com>
2024-10-21 13:38:49 +0100

    update

--- src/app/(account)/user/dashboard/(seller)/transactions-incoming/reviews/Read.tsx
+++ src/app/(account)/user/dashboard/(seller)/transactions-incoming/reviews/Read.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Image from 'next/image'
+import img1 from './img/image.png'
+import img2 from './img/image copy.png'
+import userimg1 from './img/image copy 2.png'
+import userimg2 from './img/image copy 3.png'
+
 
 const Read = () => {
+
+  const reviews = [
+    {
+      productImage: img1,
+      title: "Amazing Shoe",
+      price: "₦ 12.00",
+      userProfile: userimg2,
+      username: "Monye Fubara",
+      rating: "4.8",
+      time: "1 hr ago",
+      review: "Their timely and reliable delivery service helped us streamline our operations...",
+
+    }
+  ]
+
   return (
     <div>
-      In progress....
+      <div>
+
+      </div>
     </div>
   )
 }
