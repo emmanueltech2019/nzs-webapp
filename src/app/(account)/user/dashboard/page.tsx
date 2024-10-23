70669e0ba161d028f39bba3bb581762efbe7bb35
Emmanuel Lucky <emmanueltech2019@gmail.com>
2024-10-23 13:26:37 +0100

    updates

--- src/app/(account)/user/dashboard/page.tsx
+++ src/app/(account)/user/dashboard/page.tsx
@@ -1,49 +1,73 @@
-"use client"
-import React, { useState, Suspense } from 'react'
-import Card from '@/components/cards/ProductCard';
-import Header from '@/components/header/ProductHeader';
-import Tabs from '@/components/tabs/ProductAndServiceTab';
-import FloatingButton from '@/components/buttons/FloatingButton';
-import Carousel from '@/components/carousel/Carousel';
-import SortFilter from '@/components/SortFilter/SortFilter';
-import ProductGrid from '@/components/Grid/ProductGrid';
-import { useRouter, useSearchParams } from 'next/navigation';
-import Services from './Services';
+"use client";
+import React, { useState, Suspense, useEffect } from "react";
+import Card from "@/components/cards/ProductCard";
+import Header from "@/components/header/ProductHeader";
+import Tabs from "@/components/tabs/ProductAndServiceTab";
+import FloatingButton from "@/components/buttons/FloatingButton";
+import Carousel from "@/components/carousel/Carousel";
+import SortFilter from "@/components/SortFilter/SortFilter";
+import ProductGrid from "@/components/Grid/ProductGrid";
+import { useRouter, useSearchParams } from "next/navigation";
+import Services from "./Services";
+import axios from "@/utils/axios";
+import { ProductT } from "@/types/Product.types";
 
 const Dashboard = () => {
-  const products = [
-    { title: 'Amazing Shoes', price: '₦ 12.00' },
-    { title: 'Fabulous Shoes', price: '₦ 15.00' },
-    { title: 'Fantastic Shoes', price: '₦ 15.00' },
-    { title: 'Spectacular Shoes', price: '₦ 12.00' },
-    { title: 'Stunning Shoes', price: '₦ 12.00' },
-    { title: 'Wonderful Shoes', price: '₦ 15.00' },
-    { title: 'Stunning Shoes', price: '₦ 12.00' },
-    { title: 'Wonderful Shoes', price: '₦ 15.00' },
-  ];
+  const [products, setProducts] = useState<ProductT[]>([]); // useState expects an array of Product
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("userToken") || "";
+    const tr = JSON.parse(userToken);
+    
+    axios({
+      method: "GET",
+      url: "products",
+      headers: {
+        Authorization: `Bearer ${tr}`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        setProducts(res.data);  // Set the products once
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    
+    // Add an empty dependency array to ensure the effect only runs once
+  }, []);  // No products dependency here
+  
+
   const router = useRouter();
   const searchParams = useSearchParams();
-  const main = searchParams.get('main') || '';
-  const [activeTab, setActiveTab] = useState(main || 'products');
+  const main = searchParams.get("main") || "";
+  const [activeTab, setActiveTab] = useState(main || "products");
 
   return (
-    <div className="min-h-screen">{/*  md:w-[61vw]  */}
-      <div className=''>
-
+    <div className="min-h-screen">
+      {/*  md:w-[61vw]  */}
+      <div className="">
         <Header />
         <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
-        {activeTab == "products" ?
+        {activeTab == "products" ? (
           <>
             <SortFilter />
-            <FloatingButton color="bg-[#006838]" onClick={() => { router.push('./dashboard/add-item') }} />
+            <FloatingButton
+              color="bg-[#006838]"
+              onClick={() => {
+                router.push("./dashboard/add-item");
+              }}
+            />
             <Carousel />
-            <ProductGrid title='Perfect for you' />
-            <ProductGrid title='For this summer' /></>
-          : <>
+            <ProductGrid title="Perfect for you" products={products} />
+            <ProductGrid title="For this summer" products={products} />
+          </>
+        ) : (
+          <>
             <FloatingButton />
             <Services />
-          </>}
-
+          </>
+        )}
 
         {/* search component */}
         {/* <div className="grid grid-cols-2 py-10 sm:grid-cols-2 lg:grid-cols-2 gap-6 overflow-y-scroll	">
@@ -52,15 +76,14 @@ const Dashboard = () => {
         ))}
       </div> */}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 const page = () => (
   <Suspense fallback={<div>loading...</div>}>
     <Dashboard />
   </Suspense>
-)
+);
 
-export default page
+export default page;
---
+++ src/types/Product.types.tsx
@@ -0,0 +1,20 @@
+export interface ProductT {
+    _id: number;
+    name: string;
+    price: string;
+    description: string
+}
+
+export interface CartItemT {
+    productId: ProductT[];
+    quantity: number;
+    price: number;
+    _id: string;
+  }
+  
+  export interface CartT {
+    _id: string;   
+    items: CartItemT[]; 
+  }
+
+  
