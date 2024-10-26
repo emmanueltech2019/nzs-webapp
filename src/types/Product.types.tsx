e0d9b15d37d210177ab9899b0d23bcd2ddb0ff15
MIRACLE <udohmiracle23@gmail.com>
2024-10-26 13:56:22 +0100

    repo updated

--- src/types/Product.types.tsx
+++ src/types/Product.types.tsx
@@ -1,21 +1,20 @@
 export interface ProductT {
-    _id: number;
-    name: string;
-    price: string;
-    description: string;
-    images: string[]
+  _id: number;
+  name: string;
+  price: string;
+  description: string;
+  images: string[]
 }
 
 export interface CartItemT {
-    productId: ProductT[];
-    quantity: number;
-    price: number;
-    _id: string;
-  }
-  
-  export interface CartT {
-    _id: string;   
-    items: CartItemT[]; 
-  }
+  productId: ProductT[];
+  quantity: number;
+  price: number;
+  _id: string;
+}
 
-  
+export interface CartT {
+  _id: string;
+  items: CartItemT[];
+}
+
