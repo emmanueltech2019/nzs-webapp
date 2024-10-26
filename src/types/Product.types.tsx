cc52efd7395e621d92fbbe099ede8c1738434aba
Emmanuel Lucky <emmanueltech2019@gmail.com>
2024-10-26 13:55:37 +0100

    updates

--- src/types/Product.types.tsx
+++ src/types/Product.types.tsx
@@ -1,21 +1,32 @@
+// export interface ProductT {
+//     _id: number;
+//     name: string;
+//     price: string;
+//     description: string;
+//     images: string[]
+// }
+
+// export interface CartItemT {
+//     productId: ProductT[];
+//     quantity: number;
+//     price: number;
+//     _id: string;
+//   }
 export interface ProductT {
-    _id: number;
-    name: string;
-    price: string;
-    description: string;
-    images: string[]
+  _id: string;
+  name: string;
+  images: string[]; // Add images as an array of strings
+  price: number;
 }
 
 export interface CartItemT {
-    productId: ProductT[];
-    quantity: number;
-    price: number;
-    _id: string;
-  }
-  
-  export interface CartT {
-    _id: string;   
-    items: CartItemT[]; 
-  }
+  _id: string;
+  productId: ProductT;
+  quantity: number;
+  price: number;
+}
 
-  
+export interface CartT {
+  _id: string;
+  items: CartItemT[];
+}
