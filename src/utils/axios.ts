1c0939adfde17773e408e5e94cacf4782d547691
Emmanuel Lucky <emmanueltech2019@gmail.com>
2025-10-08 08:29:43 +0100

    updated

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-axios.defaults.baseURL = process.env.baseUrl;
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 // axios.defaults.baseURL = 'http://localhost:3000/api';
 
 
