b31ba951dd2c80d021f849435e3e72bfa9ee136f
Emmanuel Lucky <emmanueltech2019@gmail.com>
2025-11-11 19:46:27 +0100

    updated

--- src/app/(account)/user/dashboard/seller/add-product/page.tsx
+++ src/app/(account)/user/dashboard/seller/add-product/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react"
 import Main from "./_components/Main"
 
 
 const page = () => {
   return (
+    <Suspense fallback={<div>loading...</div>}>
     <div className='px-4 lg:px-[74px]'>
         <Main />
     </div>
+    </Suspense>
   )
 }
 
