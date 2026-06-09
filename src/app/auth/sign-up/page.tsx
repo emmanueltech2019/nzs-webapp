1a99f959e9643b75329eca0c70bcba605472dcd9
Emmanuel Lucky <emmanueltech2019@gmail.com>
2026-06-09 10:31:58 +0100

    updated

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react'
 import SignupContent from './_components/SignupContent'
 
 
 const page = () => {
   return (
-    <SignupContent />
+    <Suspense fallback={<div className='w-full h-screen flex items-center justify-center'><span className='text-sm text-gray-500'>Loading...</span></div>}>
+      <SignupContent />
+    </Suspense>
   )
 }
 
