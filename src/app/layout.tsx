f581f5506c5bb96518b9ca4e62b106470e890897
Emmanuel Lucky <emmanueltech2019@gmail.com>
2026-07-03 16:50:18 +0100

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Poppins } from "next/font/google"
 import TawkToChat from "@/components/Tawk";
 import dotenv from 'dotenv';
+import Script from "next/dist/client/script";
 
 dotenv.config();
 
@@ -25,6 +26,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Load gtag.js */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        />
+        
+        {/* Initialize gtag */}
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            `,
+          }}
+        />
+      </head>
       <body className={`${poppins.className} antialiased`} style={poppins.style}>
         {children}
       </body>
